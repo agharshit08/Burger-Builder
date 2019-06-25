69a3062ff1139458f29e7510ca17d5d42afd4a58
Harshit Agrawal <harshitag785@gmail.com>
2019-06-25 14:32:43 +0530

    MVC done!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class App extends Component{
-  render(){
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+class App extends Component {
+  render() {
     return (
-      <div>
-
-      </div>
+      <Layout>
+        <BurgerBuilder></BurgerBuilder>
+      </Layout>
     );
   }
 }
---
+++ src/hoc/AuxComponent.js
@@ -0,0 +1,5 @@
+// The HOC to wrap the adjacent elements.
+
+const aux = (props) => props.children;
+
+export default aux;
