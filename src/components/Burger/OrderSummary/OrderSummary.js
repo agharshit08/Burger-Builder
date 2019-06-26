4bba28a96e4642a2b84be0d40743c44286d02e66
Harshit Agrawal <harshitag785@gmail.com>
2019-06-26 12:05:25 +0530

    Performance Improved!

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import Aux from '../../../hoc/AuxComponent';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/AuxComponent/AuxComponent';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-    const ingredientSummary = Object.keys(props.ingredients)
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (<li key={igKey}>
                 <span style={{ textTransform: 'capitalize' }}>{igKey}</span>
-                : {props.ingredients[igKey]} </li>);
-        });   
-
-    return (
-        <Aux>
-            <h1>Your Order</h1>
-            <p>A delicious buger with the following ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price : $ {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    ); 
+                : {this.props.ingredients[igKey]} </li>);
+        });
+        return (
+            <Aux>
+                <h1>Your Order</h1>
+                <p>A delicious Burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price : $ {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 import BackDrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/AuxComponent';
+import Aux from '../../../hoc/AuxComponent/AuxComponent';
 
 const sideDrawer = (props) => {
 
