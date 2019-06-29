6d14abe2ff11c652af4566ab985b1e690aef4aee
Harshit Agrawal <harshitag785@gmail.com>
2019-06-29 12:41:05 +0530

    Two-way binding added in the form

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import classes from './Input.module.css';
+
+const input = (props) => {
+
+    // ...props would distribute all the normal props automatically
+    /*
+    * props is an object.
+    * {...props} is an object destructuring
+    * That means each property of props object will be applied to <input /> of <textarea></textarea> element.
+    * 
+    * For example:
+    * props = {className: 'test'}
+    * <input {...props} /> === <input className="test" />
+    */
+
+    let inputElement = null;
+
+    switch (props.elementType) {
+
+        case ('input'):
+            inputElement = <input
+                className={classes.InputElement} {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
+            break;
+
+        case ('textarea'):
+            inputElement = <textarea
+                className={classes.InputElement} {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
+            break;
+
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => {
+                        return (
+                            <option key={option.value} value={option.value}>
+                                {option.displayValue}
+                            </option>
+                        )
+                    })}
+                </select>
+            )
+            break;
+
+        default:
+            inputElement = <input
+                className={classes.InputElement} {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
+    }
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    )
+}
+
+export default input;
